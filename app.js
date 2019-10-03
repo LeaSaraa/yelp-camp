05179c19b41166d7d957aadeb3e17ffd7919da1e
Lea Agterberg <lea.sara.agterberg@gmail.com>
2019-10-03 14:20:59 +0300

    Authentication done through passport passport-local passport-local-mongoose

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
+passport = require("passport"),
+LocalStrategy = require("passport-local"),
 Campground = require("./models/campground"),
 Comment = require("./models/comment"),
+User = require("./models/user"),
 seedDB      = require("./seeds")
 
 
 mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+seedDB();
 
-
-// Campground.create(
-// {
-//   name: "Salmon Creek",
-//   image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?cs=srgb&dl=adventure-camping-feet-6757.jpg&fm=jpg",
-//   description: "This is a perfect place for those who love to go fishing"
-// },
-//   function(err, campground){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     console.log("Newly created campground: ");
-//     console.log(campground);
-//   }
-// });
+// PASSPORT
+app.use(require("express-session")({
+  secret: "Test to see if this is working okay dan",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("landing");
@@ -107,6 +108,26 @@ app.post(".campgrounds/:id/comments", function(req, res){
   });
 });
 
+// ==============
+// AUTH ROUTES
+// ==============
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
 
 app.listen(3000, function() {
   console.log('Server listening on port 3000');
